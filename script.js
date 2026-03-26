ffddda9d8cf79f42afd58fb2e6e2f9999072c5bb
karinemarcal13-pixel <karinemarcal13@gmail.com>
2026-03-26 20:59:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// Animação ao carregar
-window.onload = function() {
-  document.body.style.opacity = "1";
-};
-
-// Botão WhatsApp com mensagem pronta
+// WhatsApp
 function chamarWhats() {
-  let numero = "5543984046323"; // COLOCA SEU NÚMERO
-  let mensagem = "Olá! Gostaria de transformar meu ambiente com adesivos.";
+  let numero = "5543984046323"; // seu número
+  let msg = "Olá! Gostaria de um orçamento.";
 
-  let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem);
-
-  window.open(url, "_blank");
+  window.open(`https://wa.me/${numero}?text=${encodeURIComponent(msg)}`);
 }
+
+// ANIMAÇÃO AO SCROLL
+let elementos = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  elementos.forEach(el => {
+    let pos = el.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
